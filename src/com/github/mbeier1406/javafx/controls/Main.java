711e76b035760da7333423cbae70edf1573db40b
Martin Beier <Martin.Beier@gmx.de>
2022-06-26 19:57:01 +0200

    UI Controls: Button

--- src/com/github/mbeier1406/javafx/controls/Main.java
+++ src/com/github/mbeier1406/javafx/controls/Main.java
@@ -1,10 +1,18 @@
 package com.github.mbeier1406.javafx.controls;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -36,7 +44,8 @@ public class Main extends Application {
 		gridPane.setHgap(15);
 		gridPane.setVgap(10);
 		gridPane.add(getLabelExample(), 0, 0);
-		final var scene = new Scene(gridPane, 400, 400);
+		gridPane.add(getButtonExample(), 1, 0);
+		final var scene = new Scene(gridPane, 800, 400);
 		stage.setScene(scene);
 		stage.show();
 	}
@@ -46,21 +55,71 @@ public class Main extends Application {
 	 * <ol>
 	 * <li>Mit Text und Farbe</li>
 	 * <li>Mit Text und Bild</li>
-	 * <li>Nur mit Bild</li>
+	 * <li>Nur mit Bild und Skalierungseffekt</li>
 	 * </ol>
 	 * @return das Layout mit den Labeln
 	 * @see #start(Stage)
 	 */
 	private Pane getLabelExample() {
-		final var vBox = new VBox();
-		vBox.setSpacing(5);
-		final var label1 = new Label("Demonstriert die Nutzung von Farbe");
+		final var vBox = new VBox(5); // vBox.setSpacing(5);
+		final var label1 = new Label("Demonstriert die Nutzung von Farbe und Fonts");
 		label1.setTextFill(Color.web("#00f0a3"));
+		label1.setFont(new Font("Helvetica", 20));
 		final var label2 = new Label("GitHub", new ImageView(new Image(getClass().getResourceAsStream(IMAGES_GITHUB_PNG))));
-		label2.setFont(new Font("Helvetica", 15));
 		final var label3 = new Label();
 		label3.setGraphic(new ImageView(new Image(getClass().getResourceAsStream(IMAGES_GITHUB_PNG))));
+		label3.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				label3.setScaleX(1.5); label3.setScaleY(1.5);
+				label3.setTranslateX(20); label3.setTranslateY(20);
+			}
+		});
+		label3.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				label3.setScaleX(1); label3.setScaleY(1);
+				label3.setTranslateX(0); label3.setTranslateY(0);
+			}
+		});
 		vBox.getChildren().addAll(label1, label2, label3);
+		return vBox;
+	}
+
+	private Pane getButtonExample() {
+		final var vBox = new VBox(5); // vBox.setSpacing(5);
+		final var button1 = new Button("Verschieb mich...", new ImageView(new Image(getClass().getResourceAsStream(IMAGES_GITHUB_PNG))));
+		button1.setFont(new Font("Helvetica", 15));
+		button1.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent arg0) {
+				button1.setTranslateX(button1.getTranslateX() > 0 ? 0 : 50); 
+			}
+		});
+		final var label = new Label();
+		final var toggleGroup = new ToggleGroup();
+		toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			@Override
+			public void changed(ObservableValue<? extends Toggle> observableValue, Toggle oldValue, Toggle newValue) {
+				if ( toggleGroup.getSelectedToggle() != null )
+					label.setText((String) toggleGroup.getSelectedToggle().getUserData());
+			}
+		});
+		final var rb1 = new RadioButton("Auswahl 1");
+		final var rb2 = new RadioButton("Auswahl 2");
+		final var rb3 = new RadioButton("Auswahl 3");
+		rb3.setGraphic(new ImageView(new Image(getClass().getResourceAsStream(IMAGES_GITHUB_PNG))));
+		rb3.setSelected(true);
+		rb1.setToggleGroup(toggleGroup);
+		rb2.setToggleGroup(toggleGroup);
+		rb3.setToggleGroup(toggleGroup);
+		rb1.setUserData("Erster");
+		rb2.setUserData("Zweiter");
+		rb3.setUserData("Dritter");
+		final var rbVBox = new VBox(3);
+		rbVBox.getChildren().addAll(rb1, rb2, rb3);
+	
+		vBox.getChildren().addAll(button1, rbVBox, label);
 		return vBox;
 	}
 
