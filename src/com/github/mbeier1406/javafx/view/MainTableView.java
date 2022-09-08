cb8ed8fdf99c67d88c72365ba766f4bb8ab2fe61
Martin Beier <Martin.Beier@gmx.de>
2022-09-08 21:24:52 +0200

    Beispiel für ListView und TableView.

--- src/com/github/mbeier1406/javafx/view/MainTableView.java
+++ src/com/github/mbeier1406/javafx/view/MainTableView.java
@@ -4,13 +4,17 @@ import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TableColumn.SortType;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -32,8 +36,21 @@ public class MainTableView extends Application {
 		final var tableView =  new TableView<Person>();
 		final var emailNameCol = new TableColumn<Person, String>("E-Mail");
 		emailNameCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+		emailNameCol.setMinWidth(200);
+		emailNameCol.setMaxWidth(250);
+		emailNameCol.setSortable(true);
+		emailNameCol.setSortType(SortType.ASCENDING);
+		emailNameCol.setCellFactory(TextFieldTableCell.<Person>forTableColumn());
+		emailNameCol.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Person,String>>() {	
+			@Override
+			public void handle(CellEditEvent<Person, String> event) {
+				tableView.getItems().get(event.getTablePosition().getRow()).setEmail(event.getNewValue());
+			}
+		});
 		final var userNameCol = new TableColumn<Person, String>("Username");
 		userNameCol.setCellValueFactory(new PropertyValueFactory<>("userName"));
+		userNameCol.setMinWidth(100);
+		userNameCol.setMaxWidth(150);
 		final var firstNameCol = new TableColumn<Person, String>("Vorname");
 		firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
 		final var lastNameCol = new TableColumn<Person, String>("Nachname");
@@ -42,6 +59,7 @@ public class MainTableView extends Application {
 		fullNameCol.getColumns().addAll(firstNameCol, lastNameCol);
 		tableView.getColumns().addAll(userNameCol, fullNameCol, emailNameCol);
 		tableView.setItems(getPersonen());
+		tableView.setEditable(true);
 
 		final var vBox = new VBox();
 		vBox.setSpacing(10);
