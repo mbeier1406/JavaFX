7ffd9b81cc5e8ccd28087230f8b74b1665b800f5
Martin Beier <Martin.Beier@gmx.de>
2022-12-17 21:16:50 +0100

    Zwischenstand Kontaktformular

---
+++ src/com/github/mbeier1406/javafx/db/ControllerKontakt.java
@@ -0,0 +1,51 @@
+package com.github.mbeier1406.javafx.db;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class ControllerKontakt {
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private TextField lastNametextField;
+
+    @FXML
+    private TextField phoneNumberTextField;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void cancelButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eMailTextField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void keyReleasedProperty(KeyEvent event) {
+
+    }
+
+    @FXML
+    void loadContactData(ActionEvent event) {
+
+    }
+
+}
