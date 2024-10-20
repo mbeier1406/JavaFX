6e075114101ea774526d3d7d6210f083d8c68310
Martin Beier <Martin.Beier@gmx.de>
2024-10-20 12:31:57 +0200

    JavaFX Darstellung von Graphen

---
+++ javafx-graph/src/main/java/com/github/mbeier1406/javafx/graph/Controller.java
@@ -0,0 +1,14 @@
+package com.github.mbeier1406.javafx.graph;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Controller für {@code Application.fxml}.
+ */
+public class Controller {
+
+	@FXML
+	public BorderPane borderPane;
+
+}
--- javafx-graph/src/main/java/com/github/mbeier1406/javafx/graph/Main.java
+++ javafx-graph/src/main/java/com/github/mbeier1406/javafx/graph/Main.java
@@ -1,5 +1,8 @@
 package com.github.mbeier1406.javafx.graph;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,16 +10,25 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * Startet die ANwendung zur Anzeige von Graphen.
+ */
 public class Main extends Application {
+
+	public static final Logger LOGGER = LogManager.getLogger(Main.class);
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		final Parent root = new FXMLLoader().load(getClass().getResourceAsStream("Application.fxml"));
-		final var borderPane = new BorderPane();
+		final var fxmlLoader = new FXMLLoader();
+		final Parent root = fxmlLoader.load(getClass().getResourceAsStream("Application.fxml"));
+		final var controller = (Controller) fxmlLoader.getController();
+		LOGGER.info("Starte controller {}", controller);
+		final var scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("Application.css").toExternalForm());
 
 		primaryStage.setTitle("Graphen");
-		primaryStage.setScene(new Scene(borderPane, 100, 100));
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
